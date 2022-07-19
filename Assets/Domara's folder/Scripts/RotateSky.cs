6387804f0cd98e3301d1a40956e8a20acb0031aa
LightningBlockO <domara.oraham@gmail.com>
2022-07-19 15:28:42 +1000

    Main Menu Scene Completed

---
+++ Assets/Domara's folder/Scripts/RotateSky.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSky : MonoBehaviour
+{
+    public float rotateSpeed = 1f;
+    private void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * rotateSpeed);
+    }
+}
